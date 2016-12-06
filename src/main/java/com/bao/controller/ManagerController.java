b1881a6d89da3ac473de4dba133fee35941f7e3e
bjling <bj_ling@sina.com>
2016-12-06 09:17:58 +0800

    jquery datatable通用分页

--- src/main/java/com/bao/controller/ManagerController.java
+++ src/main/java/com/bao/controller/ManagerController.java
@@ -1,11 +1,14 @@
 package com.bao.controller;
 
+import java.net.URLDecoder;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.codec.digest.DigestUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bao.controller.msg.AllShopperResponse;
+import com.bao.controller.msg.DataTableReqInfo;
+import com.bao.controller.msg.DataTableRespInfo;
 import com.bao.controller.msg.LoginResponse;
-import com.bao.controller.msg.PageRequest;
 import com.bao.controller.msg.ShopperResponse;
 import com.bao.framework.ResponseEntity;
 import com.bao.mapper.ManagerMapper;
@@ -29,68 +32,89 @@ import com.bao.model.Shopper;
 @RequestMapping("/manager")
 public class ManagerController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ManagerController.class);
+  private static final Logger logger = LoggerFactory.getLogger(ManagerController.class);
 
-	@Autowired
-	private ManagerMapper managerMapper;
-	@Autowired
-	private ShopperMapper shopperMapper;
+  @Autowired
+  private ManagerMapper managerMapper;
+  @Autowired
+  private ShopperMapper shopperMapper;
 
-	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
 
-	@RequestMapping(value = "/common/login", method = RequestMethod.POST)
-	public ResponseEntity<?> login(@RequestBody Manager manager) {
-		try {
-			LoginResponse loginResponse = new LoginResponse();
-			manager.setPassword(DigestUtils.md5Hex(manager.getPassword()));
-			Manager managerResult = managerMapper.login(manager);
-			if (managerResult != null) {
-				String uuid = UUID.randomUUID().toString();
-				stringRedisTemplate.opsForValue().set("USER_TOKEN_" + uuid, String.valueOf(managerResult.getId()), 12,
-						TimeUnit.HOURS);
-				loginResponse.setToken(uuid);
-				loginResponse.setLoginName(managerResult.getName());
-			} else {
-				return ResponseEntity.error("登录失败", loginResponse);
-			}
-			return ResponseEntity.success(loginResponse);
-		} catch (Exception e) {
-			logger.error("error to login", e);
-			return ResponseEntity.error("登录失败", e);
-		}
-	}
+  @RequestMapping(value = "/common/login", method = RequestMethod.POST)
+  public ResponseEntity<?> login(@RequestBody Manager manager) {
+    try {
+      LoginResponse loginResponse = new LoginResponse();
+      manager.setPassword(DigestUtils.md5Hex(manager.getPassword()));
+      Manager managerResult = managerMapper.login(manager);
+      if (managerResult != null) {
+        String uuid = UUID.randomUUID().toString();
+        stringRedisTemplate.opsForValue().set("USER_TOKEN_" + uuid,
+            String.valueOf(managerResult.getId()), 12, TimeUnit.HOURS);
+        loginResponse.setToken(uuid);
+        loginResponse.setLoginName(managerResult.getName());
+      } else {
+        return ResponseEntity.error("登录失败", loginResponse);
+      }
+      return ResponseEntity.success(loginResponse);
+    } catch (Exception e) {
+      logger.error("error to login", e);
+      return ResponseEntity.error("登录失败", e);
+    }
+  }
 
-	@RequestMapping(value = "/createShopper", method = RequestMethod.POST)
-	public ResponseEntity<?> createShopper(@RequestBody Shopper shopper) {
-		try {
-			ShopperResponse createShopperResponse = shopperMapper.createShopper(shopper);
-			if (createShopperResponse == null) {
-				return ResponseEntity.error("创建商家失败", createShopperResponse);
-			}
-			return ResponseEntity.success(createShopperResponse);
-		} catch (Exception e) {
-			logger.error("error to create shopper", e);
-			return ResponseEntity.error("创建商家失败", e);
-		}
-	}
+  @RequestMapping(value = "/createShopper", method = RequestMethod.POST)
+  public ResponseEntity<?> createShopper(@RequestBody Shopper shopper) {
+    try {
+      ShopperResponse createShopperResponse = shopperMapper.createShopper(shopper);
+      if (createShopperResponse == null) {
+        return ResponseEntity.error("创建商家失败", createShopperResponse);
+      }
+      return ResponseEntity.success(createShopperResponse);
+    } catch (Exception e) {
+      logger.error("error to create shopper", e);
+      return ResponseEntity.error("创建商家失败", e);
+    }
+  }
 
-	@RequestMapping(value = "/queryAll", method = RequestMethod.GET)
-	public ResponseEntity<?> getAllInfo(HttpServletRequest request) {
-		try {
-			int pageIndex = Integer.parseInt(request.getParameter("pageIndex"));
-			int pageSize = Integer.parseInt(request.getParameter("pageSize"));
-			PageRequest pageRequest = new PageRequest();
-			pageRequest.setPageIndex(pageIndex * pageSize);
-			pageRequest.setPageSize(pageSize);
-			AllShopperResponse response = new AllShopperResponse();
-			response.setShopperInfos(shopperMapper.getAllInfo(pageRequest));
-			response.setTotal(shopperMapper.countAllShopper());
-			return ResponseEntity.success(response);
-		} catch (Exception e) {
-			logger.error("error to get all info", e);
-			return ResponseEntity.error("查询失败", e);
-		}
-	}
+  @RequestMapping(value = "/queryAll", method = RequestMethod.POST)
+  public DataTableRespInfo getAllInfo(@RequestBody String aoData) throws Exception {
+    try {
+      JSONArray jsonArray =
+          new JSONArray(URLDecoder.decode(aoData, "utf8").replaceAll("aoData=", ""));
+      JSONObject jsonObject;
+      DataTableReqInfo dataTableReqInfo = new DataTableReqInfo();
+
+      for (int i = 0; i < jsonArray.length(); i++) {
+        jsonObject = jsonArray.getJSONObject(i);
+        if ("iDisplayLength".equals(jsonObject.getString("name"))) {
+          dataTableReqInfo.setiDisplayLength(jsonObject.getInt("value"));
+        }
+        if ("iDisplayStart".equals(jsonObject.getString("name"))) {
+          dataTableReqInfo.setiDisplayStart(jsonObject.getInt("value"));
+        }
+        if ("sEcho".equals(jsonObject.getString("name"))) {
+          dataTableReqInfo.setsEcho(jsonObject.getInt("value"));
+        }
+        if ("sSearch".equals(jsonObject.getString("name"))) {
+          dataTableReqInfo.setsSearch(jsonObject.getString("value"));
+        }
+      }
+
+
+      List<Map<String, Object>> resultList = shopperMapper.getAllInfo(dataTableReqInfo);
+      DataTableRespInfo dataTableRespInfo = new DataTableRespInfo();
+      dataTableRespInfo.setAaData(resultList);
+      int count = shopperMapper.countAllShopper(dataTableReqInfo);
+      dataTableRespInfo.setiTotalDisplayRecords(count);
+      dataTableRespInfo.setiTotalRecords(count);
+      dataTableRespInfo.setsEcho(dataTableReqInfo.getsEcho());
+      return dataTableRespInfo;
+    } catch (Exception e) {
+      logger.error("error to get all info", e);
+      throw e;
+    }
+  }
 
 }
--- src/main/java/com/bao/mapper/ShopperMapper.java
+++ src/main/java/com/bao/mapper/ShopperMapper.java
@@ -1,6 +1,7 @@
 package com.bao.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -8,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.bao.controller.msg.PageRequest;
+import com.bao.controller.msg.DataTableReqInfo;
 import com.bao.controller.msg.ShopperResponse;
 import com.bao.model.Shopper;
 import com.bao.utils.CommonUtils;
@@ -19,40 +20,46 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class ShopperMapper {
-	private static final String LOGO = "@AIXU";
+  private static final String LOGO = "@AIXU";
 
-	@Autowired
-	private SqlSessionTemplate sqlSessionTemplate;
+  @Autowired
+  private SqlSessionTemplate sqlSessionTemplate;
 
-	@Transactional
-	public ShopperResponse createShopper(Shopper shopper) throws Exception{
-		ShopperResponse response = new ShopperResponse();
-		String createLoginName = CommonUtils.getRandomLoginName(shopper.getName());
-		String password = CommonUtils.getRandomLoginPassword();
-		String loginName = this.sqlSessionTemplate.selectOne("selectByName",createLoginName);
-		if (StringUtils.isNullOrEmpty(loginName)) {
-			createLoginName += "_1";
-		} else {
-			int version = Integer
-					.parseInt(loginName.substring(loginName.lastIndexOf("_") + 1, loginName.indexOf(LOGO)));
-			version++;
-			createLoginName += "_" + version;
-		}
-		shopper.setLoginName(createLoginName + LOGO);
-		shopper.setLoginPassword(DigestUtils.md2Hex(password));
-		if (this.sqlSessionTemplate.insert("createShopper", shopper) > 0) {
-			response.setUserName(shopper.getLoginName());
-			response.setPassword(password);
-			return response;
-		}
-		return null;
-	}
+  @Transactional
+  public ShopperResponse createShopper(Shopper shopper) throws Exception {
+    ShopperResponse response = new ShopperResponse();
+    String createLoginName = CommonUtils.getRandomLoginName(shopper.getName());
+    String password = CommonUtils.getRandomLoginPassword();
+    String loginName = this.sqlSessionTemplate.selectOne("selectByName", createLoginName);
+    if (StringUtils.isNullOrEmpty(loginName)) {
+      createLoginName += "_1";
+    } else {
+      int version = Integer
+          .parseInt(loginName.substring(loginName.lastIndexOf("_") + 1, loginName.indexOf(LOGO)));
+      version++;
+      createLoginName += "_" + version;
+    }
+    shopper.setLoginName(createLoginName + LOGO);
+    shopper.setLoginPassword(DigestUtils.md2Hex(password));
+    if (this.sqlSessionTemplate.insert("createShopper", shopper) > 0) {
+      response.setUserName(shopper.getLoginName());
+      response.setPassword(password);
+      return response;
+    }
+    return null;
+  }
 
-	public List<Shopper> getAllInfo(PageRequest pageRequest) {
-		return this.sqlSessionTemplate.selectList("selectAllShopper",pageRequest);
-	}
-	
-	public long countAllShopper() {
-		return this.sqlSessionTemplate.selectOne("countTotalShopper");
-	}
+
+  public long countAllShopper() {
+    return this.sqlSessionTemplate.selectOne("countTotalShopper");
+  }
+
+  public List<Map<String, Object>> getAllInfo(DataTableReqInfo dataTableReqInfo) {
+    return this.sqlSessionTemplate.selectList("selectAllShopper", dataTableReqInfo);
+  }
+
+
+  public int countAllShopper(DataTableReqInfo dataTableReqInfo) {
+    return this.sqlSessionTemplate.selectOne("countTotalShopper", dataTableReqInfo);
+  }
 }
