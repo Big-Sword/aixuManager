5ab6a452b2bc11e5399283bfbb3e9b6f35bde481
Longie <13813003522@163.com>
2016-12-12 20:07:17 +0800

    add upload product pic

--- src/main/java/com/bao/controller/ProductController.java
+++ src/main/java/com/bao/controller/ProductController.java
@@ -3,13 +3,10 @@ package com.bao.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.math.BigDecimal;
 import java.net.URLDecoder;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
@@ -27,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.bao.controller.msg.DataTableReqInfo;
 import com.bao.controller.msg.DataTableRespInfo;
+import com.bao.controller.msg.UploadResponse;
 import com.bao.framework.ResponseEntity;
 import com.bao.mapper.ProductMapper;
 import com.bao.model.Product;
@@ -43,6 +41,32 @@ public class ProductController {
 
 	@Autowired
 	private ProductMapper mapper;
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public ResponseEntity<?> upload(@RequestParam("picUrl") MultipartFile file) {
+
+		try {
+			UploadResponse uploadResponse = new UploadResponse();
+			if (!file.isEmpty()) {
+				byte[] bytes = file.getBytes();
+				String uuid = UUID.randomUUID().toString();
+				File imageFile = new File(PHOTO_PATH + uuid);
+				if (!imageFile.getParentFile().exists()) {
+					imageFile.getParentFile().mkdirs();
+				}
+				BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(imageFile));
+				outputStream.write(bytes);
+				outputStream.close();
+				uploadResponse.setPicUrl(uuid);
+			} else {
+				return ResponseEntity.error("产品图片未上传", null);
+			}
+			return ResponseEntity.success(uploadResponse);
+		} catch (Exception e) {
+			logger.error("error to saveOrUpdate product", e);
+			return ResponseEntity.error("保存或更新失败", e);
+		}
+	}
 
 	@RequestMapping(value = "/all", method = RequestMethod.POST)
 	public DataTableRespInfo getInfo(@RequestBody String aoData) throws Exception {
@@ -80,58 +104,29 @@ public class ProductController {
 	}
 
 	@RequestMapping(value = "/saveOrUpdate", method = RequestMethod.POST)
-	public ResponseEntity<?> saveOrUpdate(@RequestParam("picUrl") MultipartFile file, HttpServletRequest request) {
+	public ResponseEntity<?> saveOrUpdate(@RequestBody Product product) {
 
 		try {
-			String id = request.getParameter("id");
-			String price = request.getParameter("price");
-			String content = request.getParameter("content");
-			String name = request.getParameter("name");
-			String stock = request.getParameter("stock");
-			String modelType = request.getParameter("modelType");
-			String colourType = request.getParameter("colourType");
 
-			if (StringUtils.isBlank(name)) {
+			if (StringUtils.isBlank(product.getName())) {
 				return ResponseEntity.error("产品名称不能为空", null);
 			}
-			if (StringUtils.isBlank(price) || Double.valueOf(price) < 0) {
+			if (StringUtils.isBlank(product.getPrice().toPlainString()) || product.getPrice().doubleValue() < 0) {
 				return ResponseEntity.error("价格不能为空或者不能小于0", null);
 			}
-			if (StringUtils.isBlank(content)) {
-				return ResponseEntity.error("内存不能小于0", null);
+			if (StringUtils.isBlank(product.getContent())) {
+				return ResponseEntity.error("内容不能为空", null);
 			}
-			if (StringUtils.isBlank(stock) || Integer.parseInt(stock) < 0) {
+			if (product.getStock() < 0) {
 				return ResponseEntity.error("库存不能小于0", null);
 			}
-			if (StringUtils.isBlank(modelType)) {
+			if (StringUtils.isBlank(product.getModelType())) {
 				return ResponseEntity.error("型号不能为空", null);
 			}
-			if (StringUtils.isBlank(colourType)) {
+			if (StringUtils.isBlank(product.getColourType())) {
 				return ResponseEntity.error("色号不能为空", null);
 			}
-			Product product = new Product();
-			product.setId(id == null ? 0L : Long.parseLong(id));
-			product.setName(name);
-			product.setPrice(BigDecimal.valueOf(Double.valueOf(price)));
-			product.setContent(content);
-			product.setModelType(modelType);
-			product.setColourType(colourType);
-			product.setStock(Integer.parseInt(stock));
-			if (!file.isEmpty()) {
-				byte[] bytes = file.getBytes();
-				File imageFile = new File(PHOTO_PATH + UUID.randomUUID().toString());
-				if (!imageFile.getParentFile().exists()) {
-					imageFile.getParentFile().mkdirs();
-				}
-				BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(imageFile));
-				outputStream.write(bytes);
-				outputStream.close();
-				product.setPicUrl(imageFile.getPath());
-			} else {
-				if (id == null) {
-					return ResponseEntity.error("产品图片未上传", null);
-				}
-			}
+
 			return ResponseEntity.success(mapper.saveOrUpdate(product));
 		} catch (Exception e) {
 			logger.error("error to saveOrUpdate product", e);
---
+++ src/main/java/com/bao/controller/msg/UploadResponse.java
@@ -0,0 +1,15 @@
+package com.bao.controller.msg;
+
+public class UploadResponse {
+
+	private String picUrl;
+
+	public String getPicUrl() {
+		return picUrl;
+	}
+
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+
+}
